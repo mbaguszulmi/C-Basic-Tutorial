2b715d0049dfcc43755cf6a90d89c5e19264b20e
Muhammad Bagus Zulmi <baguszulmi101198@gmail.com>
2020-02-10 07:06:08 +0700

    cpp1 commit

---
+++ cpp1/contoh.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // cout << "Muhammad Bagus Zulmi" << endl;
+  // cout << 'A' << endl;
+  // cout << 12 << endl;
+  // cout << 12+6 << endl;
+  int angka;
+  // angka = 10;
+  // cout << angka+2 << endl;
+  cout << "Masukkan angka : ";
+  cin >> angka;
+  cout << "Angka yang dimasukkan : " << angka << endl;
+  return 0;
+}
